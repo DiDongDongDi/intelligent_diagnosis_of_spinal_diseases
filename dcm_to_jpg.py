361acdbec1f88f7a132bf95eb6391a636f21238d
DiDong <2572931653@qq.com>
2020-07-18 09:11:01 +0800

    add dcm_to_jpg

--- dcm_to_jpg.py
+++ dcm_to_jpg.py
@@ -8,6 +8,7 @@
 """
 
 import os
+import shutil
 import sys
 
 import matplotlib.pyplot as plt
@@ -15,38 +16,44 @@ import pydicom
 import scipy.misc
 
 
-def convert_dcm_to_jpg(source_file, target_file):
+def convert_dcm_to_jpg(dcm_file, jpg_file):
     """将dcm文件转为jpg文件
 
     Args:
-        source_file (str): 源文件
-        target_file (str): 目标文件
+        dcm_file (str): dcm文件
+        jpg_file (str): jpg文件
     """
 
-    ds = pydicom.read_file(source_file)
+    print("Converting {} to {}".format(dcm_file, jpg_file))
+    ds = pydicom.read_file(dcm_file)
     img = ds.pixel_array
-    scipy.misc.imsave(target_file, img)
+    scipy.misc.imsave(jpg_file, img)
 
 
-def convert_dcm_to_jpg_dir(source_dir, target_dir):
-    """将源目录中的dcm文件转换为目标目录中的jpg文件
+def convert_dcm_to_jpg_dir(dcm_dir, jpg_dir):
+    """将dcm目录中的dcm文件转换为jpg目录中的jpg文件
 
-    源目录中的非dcm文件也会被复制
+    dcm目录中的非dcm文件也会被复制
 
     Args:
-        source_dir (str): 源目录
-        target_dir (str): 目标目录
+        dcm_dir (str): dcm目录
+        jpg_dir (str): jpg目录
     """
 
-
-    walk = os.walk(target_dir)
+    shutil.copytree(dcm_dir, jpg_dir)
+    walk = os.walk(jpg_dir)
     for dir_path, __, file_names in walk:
-        
-
+        for file_name in file_names:
+            if file_name.endswith(".dcm"):
+                dcm_file_path = os.path.join(dir_path, file_name)
+                file_name = file_name.replace(".dcm", ".jpg")
+                jpg_file_path = os.path.join(dir_path, file_name)
+                convert_dcm_to_jpg(dcm_file_path, jpg_file_path)
+                os.remove(dcm_file_path)
 
 
 def option_help():
-    print("usage: dcm_to_jpg.py [source_path] [target_path]")
+    print("usage: dcm_to_jpg.py [dcm_path] [jpg_path]")
     print("dcm_to_jpg is a tool for converting dcm file to jpg file.")
 
 
@@ -54,29 +61,27 @@ def main():
     if len(sys.argv) != 3:
         option_help()
     else:
-        source_path = sys.argv[1]
-        target_path = sys.argv[2]
+        dcm_path = sys.argv[1]
+        jpg_path = sys.argv[2]
 
-        if os.path.isfile(source_path) and source_path.endswith(".dcm"):
-            if target_path.endswith(".jpg"):
-                target_parent_dir = os.path.dirname(target_path)
-                if os.path.isdir(target_parent_dir):
-                    convert_dcm_to_jpg(source_path, target_path)
+        if os.path.isfile(dcm_path) and dcm_path.endswith(".dcm"):
+            if jpg_path.endswith(".jpg"):
+                jpg_parent_dir = os.path.dirname(jpg_path)
+                if os.path.isdir(jpg_parent_dir):
+                    convert_dcm_to_jpg(dcm_path, jpg_path)
                 else:
-                    print("Error: can not find directory {}.".format(target_parent_dir))
-            elif os.path.isdir(target_path):
-                file_name = os.path.basename(source_path)
+                    print("Error: can not find directory {}.".format(jpg_parent_dir))
+            elif os.path.isdir(jpg_path):
+                file_name = os.path.basename(dcm_path)
                 file_name = file_name.replace(".dcm", ".jpg")
-                target_path = os.path.join(target_path, file_name)
-                convert_dcm_to_jpg(source_path, target_path)
+                jpg_path = os.path.join(jpg_path, file_name)
+                convert_dcm_to_jpg(dcm_path, jpg_path)
             else:
-                print("Error: can not find directory {}.".format(target_path))
-        elif os.path.isdir(source_path):
-            if not os.path.isdir(target_path):
-                os.mkdir(target_path)
-            
+                print("Error: can not find directory {}.".format(jpg_path))
+        elif os.path.isdir(dcm_path):
+            convert_dcm_to_jpg_dir(dcm_path, jpg_path)
         else:
-            print("Error: source path {} is not exist".format(source_path))
+            print("Error: dcm path {} is not exist".format(dcm_path))
 
 
 if __name__ == "__main__":
